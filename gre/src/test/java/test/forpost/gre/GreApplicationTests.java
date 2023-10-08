f7ab0118f5b8f7c73ca03b869425c064ad2dca5f
GustavEiffels <sibuk9315@gmail.com>
2023-10-08 22:57:26 +0900

    test

---
+++ gre/src/test/java/test/forpost/gre/GreApplicationTests.java
@@ -0,0 +1,13 @@
+package test.forpost.gre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
